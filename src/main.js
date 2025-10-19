f879d61ff2670427684401bfab90f11d3970f643
alexmkennell-ship-it <alexmkennell@gmail.com>
2025-10-18 19:45:06 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader.js";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import "./bob.js";
 
+// expose globals for bob.js to use
 window.THREE = THREE;
 window.FBXLoader = FBXLoader;
 window.OrbitControls = OrbitControls;
